c635133d6e3c28941b308190556442944b7145d7
Andrew <andrew.keizze@gmail.com>
2021-10-21 13:03:19 -0300

    listagem e inicio serviço

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,8 +5,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 
+import { SharedModule } from '../shared/shared.module';
+
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, RoutingModule],
+  imports: [CommonModule, RoutingModule, SharedModule],
 })
 export class PagesModule {}

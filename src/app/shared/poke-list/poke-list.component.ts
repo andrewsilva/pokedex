a1e62ce659d36359240daef62e6fec235d629f70
Andrew <andrew.keizze@gmail.com>
2021-10-22 21:58:19 -0300

    implementação search

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,13 +8,21 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
   public getAllPokemons: any;
+  private setAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe((res) => {
-      this.getAllPokemons = res.results;
-      console.log(this.getAllPokemons);
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
     });
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLocaleLowerCase());
+    });
+    this.getAllPokemons = filter;
   }
 }
